c4449868c9787794a82359fadeac3e4c5c4d27b0
Andrew <andrewmgunn26@gmail.com>
2021-08-30 08:47:18 -0500

    making changes

--- DataComposite/Program.cs
+++ DataComposite/Program.cs
@@ -7,6 +7,14 @@ namespace DataComposite
         static void Main(string[] args)
         {
             DateTime today = DateTime.Now;
+            Console.WriteLine("1. {0:MMMM dd, yyyy}", today);
+            Console.WriteLine("2. {0:yyyy.mm.dd}", today);
+            Console.Write("1. {0:MMMM dd, yyyy}", today);
+            Console.Write("1. {0:MMMM dd, yyyy}", today);
+            Console.Write("1. {0:MMMM dd, yyyy}", today);
+            Console.Write("1. {0:MMMM dd, yyyy}", today);
+            Console.Write("1. {0:MMMM dd, yyyy}", today);
+            Console.Write("1. {0:MMMM dd, yyyy}", today);
             Console.Write("1. {0:MMMM dd, yyyy}", today);
         }
     }
